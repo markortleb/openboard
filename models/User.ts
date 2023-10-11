48708923a03d738725fe18b31a474f2d43118a86
Mark Ortleb <31611567+markortleb@users.noreply.github.com>
2023-10-10 22:27:24 -0400

    create user model

---
+++ models/User.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, model, Document } from "mongoose";
+
+
+interface IUser extends Document{
+    userId: string,
+    username: string,
+    password: string,
+    createdTimestamp: string
+}
+
+
+const UserSchema = new Schema<IUser> (
+    {
+        userId: {type: String, required: true},
+        username: {type: String, required: true},
+        password: {type: String, required: true},
+        createdTimestamp: {type: String, required: true}
+    }
+);
+
+
+UserSchema.virtual('url').get( function() {
+    return '/user/' + this.userId;
+});
+
+const User = model<IUser>('user', UserSchema);
+
+
+export default User;
